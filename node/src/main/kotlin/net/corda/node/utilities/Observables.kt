c2b35b9ff2da93ed92e1c7dc37582321c0287a59
rick.parker <rick.parker@r3cev.com>
2016-12-01 15:32:44 +0000

    Unit test for export function.

--- node/src/main/kotlin/net/corda/node/utilities/Observables.kt
+++ node/src/main/kotlin/net/corda/node/utilities/Observables.kt
@@ -7,13 +7,9 @@ import rx.Observable
 fun <T : Any> Observable<T>.export(services: ServiceHub): Observable<T> {
     val stackTrace = Thread.currentThread().stackTrace
     for (frame in stackTrace) {
-        //println("class name = ${frame.className} ${AbstractNode::class.qualifiedName}")
         if (frame.className == AbstractNode::class.qualifiedName) {
-            //println("*** Result = internal")
             return this
         }
     }
-    //println("*** Result = external")
-    //println("*** Externalise ${stackTrace.joinToString("\n")}")
     return this.observeOn(services.externallyObservableScheduler)
 }
---
+++ node/src/test/kotlin/net/corda/node/utilities/ObservablesTests.kt
@@ -0,0 +1,62 @@
+package net.corda.node.utilities
+
+import com.google.common.util.concurrent.SettableFuture
+import net.corda.core.messaging.SingleMessageRecipient
+import net.corda.core.node.services.ServiceInfo
+import net.corda.core.node.services.VaultService
+import net.corda.node.services.config.NodeConfiguration
+import net.corda.testing.node.MockNetwork
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import rx.Observable
+import rx.subjects.PublishSubject
+import java.security.KeyPair
+import kotlin.test.assertNotNull
+
+
+class ObservablesTests {
+
+    @Test
+    fun `exported from within node is raw and exported from outside node is deferred`() {
+        val rawObservable = PublishSubject.create<Unit>()
+        var unexportedObservable: Observable<Unit>? = null
+
+        // Create a mock node, and then register an observer a) during start up and b) after startup.
+        val network = MockNetwork(defaultFactory = object : MockNetwork.Factory {
+            override fun create(config: NodeConfiguration, network: MockNetwork, networkMapAddr: SingleMessageRecipient?, advertisedServices: Set<ServiceInfo>, id: Int, keyPair: KeyPair?): MockNetwork.MockNode {
+                return object : MockNetwork.MockNode(config, network, networkMapAddr, advertisedServices, id, keyPair) {
+                    override fun makeVaultService(): VaultService {
+                        // Export from within node.
+                        unexportedObservable = rawObservable.export(services)
+                        return super.makeVaultService()
+                    }
+                }
+            }
+        })
+        val node = network.createSomeNodes(numPartyNodes = 1).partyNodes[0]
+        // Export from outside node.
+        val exportedObservable: Observable<Unit> = rawObservable.export(node.services)
+
+        // Setup listeners and then send observation.
+        var eventSeqNo = 0
+        val exportedEventSeqNo = SettableFuture.create<Pair<Int, Boolean>>()
+        val unexportedEventSeqNo = SettableFuture.create<Pair<Int, Boolean>>()
+        val mainThread = Thread.currentThread()
+
+        assertNotNull(unexportedObservable, "unexportedObservable is null")
+        exportedObservable.subscribe { exportedEventSeqNo.set(eventSeqNo++ to (Thread.currentThread() == mainThread)) }
+        unexportedObservable!!.subscribe { unexportedEventSeqNo.set(eventSeqNo++ to (Thread.currentThread() == mainThread)) }
+
+        assertThat(rawObservable).isNotEqualTo(exportedObservable)
+        assertThat(rawObservable).isEqualTo(unexportedObservable)
+
+        rawObservable.onNext(Unit)
+
+        // Unexported should fire first and be on main thread as it is synchronous.
+        assertThat(unexportedEventSeqNo.get()).isEqualTo(0 to true)
+        // Exported should fire second and not be on main thread as asynchronous on server thread.
+        assertThat(exportedEventSeqNo.get()).isEqualTo(1 to false)
+
+        network.stopNodes()
+    }
+}
