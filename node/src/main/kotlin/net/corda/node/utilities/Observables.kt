63f3bb4254fb6c66f6f24ef1219fcc1cc90bddae
rick.parker <rick.parker@r3cev.com>
2016-12-01 16:52:29 +0000

    Improved kdocs.

--- node/src/main/kotlin/net/corda/node/utilities/Observables.kt
+++ node/src/main/kotlin/net/corda/node/utilities/Observables.kt
@@ -4,6 +4,15 @@ import net.corda.core.node.ServiceHub
 import net.corda.node.internal.AbstractNode
 import rx.Observable
 
+/**
+ * Select an appropriate [Scheduler] to deliver observations on, dependent on whether the subscriber is inside or
+ * outside the node, and return an [Observable] that observes on that [Scheduler].
+ *
+ * Those outside the node should be notified after any [Flow] has checkpointed and committed the associated database
+ * transaction so that changes are externally visible.  Otherwise the state associated with the observation might not
+ * yet have changed (been committed) to reflect it. If within the node, then it's okay to execute immediately on the
+ * [Fiber] and/or within the same database transaction.
+ */
 fun <T : Any> Observable<T>.export(services: ServiceHub): Observable<T> {
     val stackTrace = Thread.currentThread().stackTrace
     for (frame in stackTrace) {
